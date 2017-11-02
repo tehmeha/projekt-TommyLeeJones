5c553bc19070efb0f507b8ff5d2fc748d7080e05
TommyLeeJones <antezelenika12@gmail.com>
2017-11-02 11:25:24 +0100

    Šah Zelenika

--- program.cpp
+++ program.cpp
@@ -1,7 +1,43 @@
-+#include<iostream>
-+#using namespace std;
+#include<iostream>
+using namespace std;
 
-+int main()
-+{
-+         return 0;
-+}
+int main()
+{
+    int polje[8][8]={{0,0,0,0,0,0,21,0},
+             {0,0,26,14,0,0,0,15},
+             {0,0,0,26,0,0,0,0},
+             {25,0,26,0,0,13,0,0},
+             {22,0,26,0,0,0,0,0},
+             {0,25,0,0,0,0,13,11},
+             {0,0,0,0,0,12,0,0},
+             {25,24,0,0,0,0,15,0}};
+    for (int i=0;i<8;i++)
+    {
+        for(int j=0;j<8;j++)
+        {
+            if( polje[i][j] == 11 )
+                cout<<"K1"<<"\t";
+            else if( polje[i][j] == 21 )
+                cout<<"K2"<<"\t";
+        }cout << endl;
+     }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
